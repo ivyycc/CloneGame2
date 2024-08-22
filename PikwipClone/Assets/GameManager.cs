1edf0e011568a148f600b3385743a0270087c5b8
Ivy Ch <ivy.chepkwony@gmail.com>
2024-08-22 23:51:53 +0200

    fixed scene transitions

---
+++ PikwipClone/Assets/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void MoveToScene(int sceneID)
+    {
+       // AudioManager.instance.PlayOneShot(FMODEvents.instance.play, this.transform.position);
+        SceneManager.LoadScene(sceneID, LoadSceneMode.Single);
+    }
+    public void StartGame()
+    {
+        //AudioManager.instance.PlayOneShot(FMODEvents.instance.play, this.transform.position);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+    public void QuitGame()
+    {
+        //AudioManager.instance.PlayOneShot(FMODEvents.instance.quit, this.transform.position);
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+      // AudioManager.instance.PlayOneShot(FMODEvents.instance.select, this.transform.position);
+    }
+    private void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.buildIndex == 1 && Input.GetKeyDown(KeyCode.X))
+        {
+            MoveToScene(2);
+        }
+
+    }
+}
